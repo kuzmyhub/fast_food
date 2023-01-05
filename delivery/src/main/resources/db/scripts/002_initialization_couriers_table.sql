7ab91c1b89c6f5149244c0d6c0c06b59ffef854c
Sergey <kuznetsov.s.i@bk.ru>
2023-01-05 16:18:40 +0300

    Добавлено описание проекта в README.md

--- delivery/src/main/resources/db/scripts/002_initialization_couriers_table.sql
+++ delivery/src/main/resources/db/scripts/002_initialization_couriers_table.sql
@@ -1,5 +1,5 @@
-INSERT INTO couriers(name, phone) VALUES
-('Evgeniy', '88006008050'),
-('Anna', '86009995522'),
-('Oleg', '83005556070')
+INSERT INTO couriers(name, number, dismissed) VALUES
+('Evgeniy', '88006008050', false),
+('Anna', '86009995522', false),
+('Oleg', '83005556070', false)
 ON CONFLICT DO NOTHING;
