bdba5aef27b9a0ab73a3621a4353499994860e83
Sergey <kuznetsov.s.i@bk.ru>
2023-01-06 14:42:55 +0300

    Изменено описание проекта в README.md

--- client/src/main/java/ru/job4j/client/ClientMain.java
+++ client/src/main/java/ru/job4j/client/ClientMain.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"ru.job4j.domain.model", "ru.job4j.client"})
@@ -12,9 +11,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class ClientMain {
     public static void main(String[] args) {
         SpringApplication.run(ClientMain.class, args);
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        String psw = encoder.encode("password");
-        System.out.println(psw);
         System.out.println("http://localhost:8090/client/login");
     }
 }
